1bf22e51a65246786678470827b87d85ec105657
kakao014 <63865956+kakao014@users.noreply.github.com>
2023-05-29 21:14:49 +0800

    Update page.tsx

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Features from "./Features";
 import Hero from "./Hero";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function HomePage() {
   if (process.env.NEXT_PUBLIC_ENV === "local") {
@@ -9,6 +10,12 @@ export default function HomePage() {
 
   return (
     <main className="">
+      <h1>test test</h1>
+          <Link href="/upload">
+                blog
+            </Link>
+        <br/>
+            <br/>
       <Hero />
       <Features />
     </main>
