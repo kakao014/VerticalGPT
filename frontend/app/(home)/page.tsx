8cea9150e928d10e09d69722f6c09532dc632bd0
kakao014 <63865956+kakao014@users.noreply.github.com>
2023-06-01 12:04:01 +0800

    Update page.tsx

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,11 +1,12 @@
-
 import { redirect } from "next/navigation";
 import Features from "./Features";
 import Hero from "./Hero";
 
 export default function HomePage() {
   if (process.env.NEXT_PUBLIC_ENV === "local") {
+    
     redirect("/upload");
+    
   }
 
   return (
